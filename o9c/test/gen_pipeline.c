91657678d7aacbc70e5c0f7557ed674c92115762
Scott J Guyton <kiljoy001@gmail.com>
2026-05-07 23:29:51 -0700

    clean: remove stale build artifacts, add .gitignore, regenerate pipeline output

--- o9c/test/gen_pipeline.c
+++ o9c/test/gen_pipeline.c
@@ -31,6 +31,7 @@ typedef struct Counter_AsmTable {
 
 typedef struct Counter_Client {
 	int fd;
+	void *shm_base;
 	o9_AsmTable *table;
 	long ref;	/* ARC Counter */
 	void *dispatch_chan;
@@ -50,7 +51,6 @@ static void o9_impl_Counter_Counter(Counter_Internal *self, O9Msg *msg) {
 	O9Reply *r = mallocz(sizeof(O9Reply), 1);
 	vlong n = ((vlong*)msg->args)[0];
 	self->val = n;
-done:
 	r->ok = 1;
 	sendp(msg->replyc, r);
 }
@@ -68,16 +68,6 @@ static void o9_impl_Counter_inc(Counter_Internal *self, O9Msg *msg) {
 	O9Reply *r = mallocz(sizeof(O9Reply), 1);
 	vlong n = ((vlong*)msg->args)[0];
 	self->val = (self->val + n);
-done:
-	r->ok = 1;
-	sendp(msg->replyc, r);
-}
-
-static void o9_impl_Counter_double(Counter_Internal *self, O9Msg *msg) {
-	O9Reply *r = mallocz(sizeof(O9Reply), 1);
-	r->ret = (void*)((self->val * 2));
-	goto done;
-done:
 	r->ok = 1;
 	sendp(msg->replyc, r);
 }
@@ -97,7 +87,6 @@ static void Counter_loop(void *v) {
 		case 0x34ada145: o9_impl_Counter_Counter(self, m); break;
 		case 0xfdcb98a2: o9_impl_Counter_getValue(self, m); break;
 		case 0xb88801f: o9_impl_Counter_inc(self, m); break;
-		case 0xf93d5b20: o9_impl_Counter_double(self, m); break;
 		case 0x97af1ef: o9_cleanup_Counter(self); threadexits(nil); break;
 		default: { O9Reply *r = mallocz(sizeof(O9Reply), 1); r->err = "bad selector"; sendp(m->replyc, r); } break;
 		}
@@ -106,24 +95,56 @@ static void Counter_loop(void *v) {
 
 static void fsread_Counter(Req *r) {
 	char buf[1024];
-	Counter_Internal *s = r->srv->aux;
 	char *name = r->fid->file->name;
+	Counter_Internal *inst = r->fid->file->aux;
 
 	if(strcmp(name, "status") == 0) { readstr(r, "running"); respond(r, nil); return; }
+	if(inst == nil) { respond(r, "clone read"); return; }
+
 	if(strcmp(name, "val") == 0){
-		snprint(buf, sizeof buf, "%lld\n", (vlong)s->val);
+		snprint(buf, sizeof buf, "%lld\n", (vlong)inst->val);
 		readstr(r, buf); respond(r, nil); return;
 	}
 	respond(r, "not found");
 }
 
 static void fswrite_Counter(Req *r) {
-	Counter_Internal *s = r->srv->aux;
 	char *name = r->fid->file->name;
-	if(strcmp(name, "ctl") == 0) { /* TODO: parse text ctl */ respond(r, nil); return; }
-	if(strcmp(name, "msg") == 0) { /* TODO: parse binary msg */ respond(r, nil); return; }
+	Counter_Internal *inst = r->fid->file->aux;
+	if(strcmp(name, "ctl") == 0) { /* TODO: parse ctl */ respond(r, nil); return; }
+	if(strcmp(name, "Counter") == 0){
+		vlong __wargs[1] = {0};
+		__wargs[0] = strtoll(r->ifcall.data, nil, 0);
+		{ O9Msg __wm = {0x34ada145, __wargs, 1, chancreate(sizeof(void*), 0)};
+		sendp(inst->dispatch_chan, &__wm);
+		recvp(__wm.replyc);
+		chanfree(__wm.replyc); }
+		r->ofcall.count = r->ifcall.count;
+		respond(r, nil);
+		return;
+	}
+	if(strcmp(name, "getValue") == 0){
+		{ O9Msg __wm = {0xfdcb98a2, nil, 0, chancreate(sizeof(void*), 0)};
+		sendp(inst->dispatch_chan, &__wm);
+		recvp(__wm.replyc);
+		chanfree(__wm.replyc); }
+		r->ofcall.count = r->ifcall.count;
+		respond(r, nil);
+		return;
+	}
+	if(strcmp(name, "inc") == 0){
+		vlong __wargs[1] = {0};
+		__wargs[0] = strtoll(r->ifcall.data, nil, 0);
+		{ O9Msg __wm = {0xb88801f, __wargs, 1, chancreate(sizeof(void*), 0)};
+		sendp(inst->dispatch_chan, &__wm);
+		recvp(__wm.replyc);
+		chanfree(__wm.replyc); }
+		r->ofcall.count = r->ifcall.count;
+		respond(r, nil);
+		return;
+	}
 	if(strcmp(name, "val") == 0){
-		s->val = (vlong)strtoll(r->ifcall.data, nil, 0);
+		inst->val = (vlong)strtoll(r->ifcall.data, nil, 0);
 		r->ofcall.count = r->ifcall.count;
 		respond(r, nil);
 		return;
@@ -132,21 +153,28 @@ static void fswrite_Counter(Req *r) {
 }
 
 Srv o9srv_Counter;
-
+static Tree *Counter_tree;
+int Counter_create_instance(Counter_Internal *inst, char *name) {
+	File *dir = createfile(Counter_tree->root, name, nil, 0755, nil);
+	if(dir == nil) return -1;
+	dir->aux = inst;
+	createfile(dir, "status", nil, 0444, nil);
+	{ File *__f = createfile(dir, "val", nil, 0666, nil); if(__f) __f->aux = inst; }
+	{ File *__f = createfile(dir, "Counter", nil, 0222, nil); if(__f) __f->aux = inst; }
+	{ File *__f = createfile(dir, "getValue", nil, 0222, nil); if(__f) __f->aux = inst; }
+	{ File *__f = createfile(dir, "inc", nil, 0222, nil); if(__f) __f->aux = inst; }
+	return 0;
+}
 void o9_main_Counter(int argc, char **argv) {
 	Counter_Internal *s = emalloc9p(sizeof(Counter_Internal));
 	memset(s, 0, sizeof(Counter_Internal));
 	s->dispatch_chan = chancreate(sizeof(void*), 10);
 	o9srv_Counter.read = fsread_Counter;
 	o9srv_Counter.write = fswrite_Counter;
-	o9srv_Counter.aux = s;
-	Tree *t = alloctree(nil, nil, 0555, nil);
-	o9srv_Counter.tree = t;
-	createfile(t->root, "ctl", nil, 0222, nil);
-	createfile(t->root, "msg", nil, 0222, nil);
-	createfile(t->root, "status", nil, 0444, nil);
-	createfile(t->root, "cache", nil, 0444, nil);
-	createfile(t->root, "val", nil, 0666, nil);
+	Counter_tree = alloctree(nil, nil, 0555, nil);
+	o9srv_Counter.tree = Counter_tree;
+	createfile(Counter_tree->root, "clone", nil, 0222, nil);
+	createfile(Counter_tree->root, "status", nil, 0444, nil);
 	proccreate(Counter_loop, s, 8192);
 	threadpostmountsrv(&o9srv_Counter, "Counter", nil, MREPL);
 }
@@ -162,11 +190,14 @@ threadmain(int argc, char **argv)
 	c.dispatch_chan = __c->dispatch_chan;
 	__c->val = 0;
 	proccreate(Counter_loop, __c, 8192);
-	o9_call_args[0]=10;
-	obj9_msgSend(&c, 0x34ada145, o9_call_args);
+	Counter_create_instance(__c, "c");
+	{ vlong __a[1];
+	__a[0] = 10;
+	obj9_msgSend(&c, 0x34ada145, __a); }
 	(o9_call_args[0]=5, (vlong)obj9_msgSend(&c, 0xb88801f, o9_call_args));
+	while((((vlong)obj9_msgSend(&c, 0xfdcb98a2, o9_call_args)) < 1000)){
 	(o9_call_args[0]=1, (vlong)obj9_msgSend(&c, 0xb88801f, o9_call_args));
-	vlong v = ((vlong)obj9_msgSend(&c, 0xfdcb98a2, o9_call_args));
-	vlong d = ((vlong)obj9_msgSend(&c, 0xf93d5b20, o9_call_args));
+	}
+	print("done");
 	threadexitsall(nil);
 }
